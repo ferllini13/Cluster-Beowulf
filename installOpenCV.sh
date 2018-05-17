6cab7897680c7198db44829617679a0089c84f91
Frander Granados <fgranados@ic-itcr.ac.cr>
2018-05-16 18:15:00 -0600

    se agregan dependencias

--- installOpenCV.sh
+++ installOpenCV.sh
@@ -27,7 +27,10 @@ else
 fi
 
 echo "instalando dependencias"
-sudo apt-get update && sudo apt-get install cmake gcc g++
+sudo apt-get update
+sudo apt-get install build-essential cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev
+sudo apt-get install python3.5-dev python3-numpy libtbb2 libtbb-dev
+sudo apt-get install libjpeg-dev libpng-dev libtiff5-dev libjasper-dev libdc1394-22-dev libeigen3-dev libtheora-dev libvorbis-dev libxvidcore-dev libx264-dev sphinx-common libtbb-dev yasm libfaac-dev libopencore-amrnb-dev libopencore-amrwb-dev libopenexr-dev libgstreamer-plugins-base1.0-dev libavutil-dev libavfilter-dev libavresample-dev
 
 echo "Se descargan paquetes de OpenCV para la instalacion"
 wget -O opencv.zip https://github.com/opencv/opencv/archive/3.4.1.zip
@@ -41,11 +44,11 @@ unzip opencv_contrib.zip
 mv opencv_contrib-3.4.1/ opencv_contrib
 sudo mv opencv_contrib /opt/
 cd /opt/opencv
-mkdir release && cd release
-sudo cmake -D BUILD_TIFF=ON -D WITH_CUDA=OFF -D ENABLE_AVX=OFF -D WITH_OPENGL=OFF -D WITH_OPENCL=OFF -D WITH_IPP=OFF -D WITH_TBB=ON -D BUILD_TBB=ON -D WITH_EIGEN=OFF -D WITH_V4L=OFF -D WITH_VTK=OFF -D BUILD_TESTS=OFF -D BUILD_PERF_TESTS=OFF -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D OPENCV_EXTRA_MODULES_PATH=/opt/opencv_contrib/modules /opt/opencv/
-sudo make -j4
-sudo make install
-sudo ldconfig
+mkdir -p /opt/opencv/release
+cmake -D BUILD_TIFF=ON -D WITH_CUDA=OFF -D ENABLE_AVX=OFF -D WITH_OPENGL=OFF -D WITH_OPENCL=OFF -D WITH_IPP=OFF -D WITH_TBB=ON -D BUILD_TBB=ON -D WITH_EIGEN=OFF -D WITH_V4L=OFF -D WITH_VTK=OFF -D BUILD_TESTS=OFF -D BUILD_PERF_TESTS=OFF -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D OPENCV_EXTRA_MODULES_PATH=/opt/opencv_contrib/modules /opt/opencv/
+make -j4
+make install
+ldconfig
 echo "verificando version instalada"
 pkg-config --modversion opencv
 echo "instalacion terminada"
