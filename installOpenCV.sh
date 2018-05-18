95a8238ca1ea8cf669fa91c800b77998b6ecfa57
Frander Granados <fgranados@ic-itcr.ac.cr>
2018-05-18 14:43:38 -0600

    version final

--- installOpenCV.sh
+++ installOpenCV.sh
@@ -43,8 +43,8 @@ sudo mv opencv /opt/
 unzip opencv_contrib.zip
 mv opencv_contrib-3.4.1/ opencv_contrib
 sudo mv opencv_contrib /opt/
-cd /opt/opencv
 mkdir -p /opt/opencv/release
+cd /opt/opencv/release
 cmake -D BUILD_TIFF=ON -D WITH_CUDA=OFF -D ENABLE_AVX=OFF -D WITH_OPENGL=OFF -D WITH_OPENCL=OFF -D WITH_IPP=OFF -D WITH_TBB=ON -D BUILD_TBB=ON -D WITH_EIGEN=OFF -D WITH_V4L=OFF -D WITH_VTK=OFF -D BUILD_TESTS=OFF -D BUILD_PERF_TESTS=OFF -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D OPENCV_EXTRA_MODULES_PATH=/opt/opencv_contrib/modules /opt/opencv/
 make -j4
 sudo make install
